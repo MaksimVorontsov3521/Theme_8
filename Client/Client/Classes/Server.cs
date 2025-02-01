6379fc29dd6b350f696d5ec83f885a9405bafd78
MaksimVorontsov3521 <shisafanyn@gmail.com>
2025-02-01 13:27:39 +0300

    Соединение между приложениями
    Устанавливается соединение между клиентским и серверным приложениями через TCP stream

---
+++ Client/Client/Classes/Server.cs
@@ -0,0 +1,52 @@
+﻿using Client.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Reflection.PortableExecutable;
+
+namespace Client
+{
+    public class Server
+    {
+        MainWindow mainWindow;
+        Socket soket; 
+        string ip = "127.0.0.1";
+        int port = 8080;
+        public Server(MainWindow mainWindow)
+        {
+            // Связка окна и сервера
+            this.mainWindow = mainWindow;
+            Server server = this;
+            Entrance entrance = new Entrance(server);
+            mainWindow.WorkPlace.Navigate(entrance);
+        }
+
+        public void Connection(string login, string password)
+        {
+            SendBytes(login+"\a"+password);
+        }
+
+        public void SendBytes(string message)
+        {
+            // Преобразуем строку в массив байтов
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            // Отправляем массив байтов другой программе
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Connect(ip, port);
+                socket.Send(bytes);
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    socket.Send(bytes);
+                }
+            }
+
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using Server;
+using System.Net.Sockets;
+
+
+public class Program
+{
+    int port = 8080;
+    TcpListener server;
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Setup();
+        program.Connection();    
+    }
+
+    private void Setup()
+    {
+        server = new TcpListener(port);
+        server.Start();       
+    }
+    private void Connection()
+    {
+        Thread thread = new Thread(() => HandelClient());
+        Console.WriteLine("123");
+        thread.Start();
+    }
+
+    private void HandelClient()
+    {     
+        using (var client = server.AcceptTcpClient())
+        {
+            Connection();
+            // Создаем поток для чтения
+            using (var stream = client.GetStream())
+            {
+                // Читаем массив байтов из сокета
+                byte[] bytes = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = stream.Read(bytes, 0, bytes.Length)) > 0)
+                {
+                    Console.WriteLine("Читанные байты: " + BitConverter.ToString(bytes, 0, bytesRead));
+                }
+            }
+
+        }
+    }
+}
+
+
